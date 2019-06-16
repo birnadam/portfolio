9e816452430bed1b8bfbff1962538f21fd634344
birna <briandam26@yahoo.com>
2019-06-15 22:56:35 -0700

    made changes to template with some of my own text and pictures

---
+++ js/subscriberDB.js
@@ -0,0 +1,13 @@
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyATYWoZpGO6PqgpK6EG-yKAiSsVVQCjVAY",
+        authDomain: "take5in5numbers.firebaseapp.com",
+        databaseURL: "https://take5in5numbers.firebaseio.com",
+        projectId: "take5in5numbers",
+        storageBucket: "take5in5numbers.appspot.com",
+        messagingSenderId: "778611971970",
+        appId: "1:778611971970:web:5ee0a73e4b060556"
+      };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
